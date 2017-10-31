207814ea1e7761e13d0e057d14de2f17f104f7e8
Harry Manchanda <harmanmanchanda182@gmail.com>
2017-10-31 17:40:03 +0530

    Create a modal component!

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="grid-x grid-padding-x align-center-middle text-center" style="height: 200px;">
+      <div class="cell small-6">
+        <a class="button primary radius" @click="changePicture">Change Picture</a>
+      </div>
+    </div>
+     <transition>
+      <app-modal v-if="showChangePictureModal" @close="removeChangePicture"></app-modal>
+    </transition>
+  </div>
+</template>
+
+<script>
+import AppModal from './components/AppModal.vue';
+
+export default {
+  name: 'app',
+  components: {
+    'app-modal': AppModal,
+  },
+  data() {
+    return {
+      showChangePictureModal: false, 
+    };
+  },
+  methods: {
+    changePicture() {
+      this.showChangePictureModal = true;
+    },
+    removeChangePicture() {
+      this.showChangePictureModal = false;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/AppChangePicture.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="c-AppChangePicture">
+    <h1>Awesome. I Have It.</h1>
+    <p class="lead">Your couch. It is mine.</p>
+    <p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe voluptate, natus possimus dolore itaque, atque, molestiae veritatis dicta quasi eaque maxime? Ut accusantium molestiae deleniti a amet beatae nobis eaque.</p>
+    <button class="close-button" data-close aria-label="Close modal" type="button">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+
+<script>
+</script>
